de0f9f4008def1a8d09781f91ea80303d83c2a06
marcosfnmr <marcosfnmr@gmail.com>
2023-12-30 19:56:29 +0100

    añado el menu

---
+++ app/src/main/java/clases/Main.kt
@@ -0,0 +1,12 @@
+package clases
+
+class Main {
+
+    fun main(){
+        var h: ArrayList<String> = ArrayList<String>()
+        var book1: Book = Book("hola", h, 3, "hola", 3, "hola")
+
+    }
+
+
+}
