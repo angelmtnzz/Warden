e1c58a408966cbcefade66c4b45847b369d8f404
angelmtnzz <amartf29@estudiantes.unileon.es>
2024-01-02 14:09:42 +0100

    Base Search

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -67,7 +67,8 @@ dependencies {
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.navigation:navigation-fragment-ktx:2.7.6")
-    implementation("androidx.navigation:navigation-ui-ktx:2.7.6") //
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.6")
+    implementation("com.android.car.ui:car-ui-lib:2.5.1") //
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/vistas/SearchFragment.kt
+++ app/src/main/java/vistas/SearchFragment.kt
@@ -1,4 +1,25 @@
 package vistas
 
-class SearchFragment {
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import java.clases.R
+import java.clases.databinding.FragmentMainPageBinding
+
+class SearchFragment : Fragment(R.layout.fragment_search){
+
+    private lateinit var binding : FragmentMainPageBinding
+
+    override fun onCreate(savedInstanceState : Bundle?){
+        super.onCreate(savedInstanceState)
+        binding = FragmentMainPageBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreateView(inflater:LayoutInflater, container:ViewGroup?, savedInstancesState: Bundle?): View? {
+        binding.tvTexto.text= "Search Fragment"
+        return inflater.inflate(R.layout.fragment_search, container, false)
+    }
+
 }
