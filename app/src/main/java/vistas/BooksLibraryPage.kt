de876d2de26e15ee83ff57bafdb03db89ea97aa2
marcosfnmr <marcosfnmr@gmail.com>
2024-01-12 18:59:44 +0100

    Actualizo Categories en la library

---
+++ app/src/main/java/vistas/BooksLibraryPage.kt
@@ -0,0 +1,12 @@
+package vistas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import java.clases.R
+
+class BooksLibraryPage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_books_library_page)
+    }
+}
