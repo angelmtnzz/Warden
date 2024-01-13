bf200d0ec1060e915f9a77dce596e6f47986e5f8
marcosfnmr <marcosfnmr@gmail.com>
2024-01-13 23:19:46 +0100

    Dao de Books completo

--- app/src/main/java/modelos/BookDao.kt
+++ app/src/main/java/modelos/BookDao.kt
@@ -2,6 +2,7 @@ package modelos
 
 import androidx.lifecycle.LiveData
 import androidx.room.Dao
+import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
@@ -15,7 +16,28 @@ interface BookDao {
     @Query("SELECT * FROM Book")
     fun getAllBooks(): LiveData<List<Book>>
 
-    //@Query("SELECT cover FROM book WHERE bookId = :id")
+    @Query("SELECT cover FROM book WHERE titleId = :id")
+    fun getBookCover(id: Int): LiveData<Int>            //Imagenes se tratan como int
+    @Query("SELECT cover FROM book WHERE name = :name")
+    fun getBookCover(name: String): LiveData<Int>       //Imagenes se tratan como int
 
+    @Query("SELECT name FROM book WHERE titleId = :id")
+    fun getBookName(id: Int): LiveData<String>
+    @Query("SELECT id FROM book WHERE name= :name")
+    fun getBookId(name: String): LiveData<String>
 
+    @Query("SELECT author FROM book WHERE titleId = :id")
+    fun getBookAuthor(id: Int): LiveData<String>
+    @Query("SELECT author FROM book WHERE name= :name")
+    fun getBookAuthor(name: String): LiveData<String>
+
+    @Query("SELECT pages FROM book WHERE titleId = :id")
+    fun getBookPages(id: Int): LiveData<String>
+    @Query("SELECT pages FROM book WHERE name= :name")
+    fun getBookPages(name: String): LiveData<String>
+
+    @Query("SELECT genre FROM book WHERE titleId = :id")
+    fun getBookGenre(id: Int): LiveData<String>
+    @Query("SELECT genre FROM book WHERE name= :name")
+    fun getBookGenre(name: String): LiveData<String>
 }
